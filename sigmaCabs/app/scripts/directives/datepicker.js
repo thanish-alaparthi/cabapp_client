144099b23ef582d1ce4a95607e54ecd2e183b717
Sameer <sayyagari@innominds.com>
2014-01-31 00:29:06 +0530

    date picker files added

---
+++ sigmaCabs/app/scripts/directives/datepicker.js
@@ -0,0 +1,36 @@
+angular.module('sigmaCabsApp')
+  .directive('datepicker', function(PrerequisiteService, $compile) {
+    return {
+      //  require: 'ngModel',
+      link: function(scope, element, attrs, model) {
+        element
+          .datePicker({
+            createButton: false,
+            displayClose: true,
+            closeOnSelect: true,
+            selectMultiple: false
+          })
+          .bind(
+            'click',
+            function() {
+              $(this).dpDisplay();
+              this.blur();
+              return false;
+            }
+        ).bind(
+          'dateSelected',
+          function(e, selectedDate, $td, state) {
+            console.log('>>>>>',state);
+            if(!state){
+              setTimeout(function(){
+                console.log($td);
+                $td.removeClass('selected');
+              },100);
+            }
+
+          }
+        );
+
+      }
+    };
+  });
