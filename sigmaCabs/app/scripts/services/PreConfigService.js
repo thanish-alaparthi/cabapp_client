777b7fcec1e05ca1d26e57adfbcec0fd93babd23
sigma5 <sigma5@sigma5-desktop.(none)>
2014-02-01 21:18:09 +0530

    added preConfigService

---
+++ sigmaCabs/app/scripts/services/PreConfigService.js
@@ -0,0 +1,25 @@
+/*
+Name: PreConfigService
+Description: Service which handles REST Calls for PreConfigService
+Date: 1Feb2014
+Author: Mario::216mario216@gmail.com
+*/
+
+'use strict';
+
+angular.module('sigmaCabsApp')
+    .factory('PreConfigService', function($http, URLService, $rootScope) {
+        
+        return {
+            /* CONSTANTS DELARATIONS FOR BOOKING */
+            BOOKING_ENQUIRY : 1,
+            BOOKING_FOLLOW_UP : 2,
+            BOOKING_REJECTED : 3,
+            BOOKING_YET_TO_DISPATCH : 4,
+            BOOKING_VEHICLE_ASSIGNED : 5,
+            WHILE_DRIVING : 6,
+            BOOKING_COMPLETED_N_CLOSED : 7,
+            BOOKING_CANCELLED : 8
+        };
+
+    });
