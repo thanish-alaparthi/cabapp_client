4f3560dc8fdaa1f82dd1121e254642cf05a7c77c
Thanish Alaparthi <thanish@thanish.com>
2014-02-18 20:04:09 +0530

    adding serverService, a single entry and exit commnunition to handle POST and GET requests

---
+++ sigmaCabs/app/scripts/services/serverService.js
@@ -0,0 +1,47 @@
+//Data model to communicate with server
+angular.module('sigmaCabsApp')
+.factory('serverService', function($rootScope, $http){
+	var baseUrl = "";
+	var serverService = function(data){
+		angular.extend(this, data);
+	}
+	serverService.sendData = function(method, url, data, sucessCallback, errorCallback){
+		var methods = {'P':'POST','G':'GET'};
+		$http({
+				method: methods[method] || 'POST', 
+				url: baseUrl+url, 
+				data:data, 
+				headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+			})
+			.success(function(data, status, headers, config) {
+				if(status == 200){
+					//check for the valid format of the data, else through into the error callback
+					/*var isValidData = (data && data.response && data.response.stat) || false;
+						if((isValidData) && (data.response.stat == 'SUCCESS')  ){*/
+							sucessCallback(data);
+						/*} else{
+							errorCallback(data);
+						}*/
+				}else{
+					errorCallback(data);
+				}
+		  })
+		  .error(function(data, status, headers, config) {
+		  	/*
+		  		May be we need to ask for header 401 from server, if login session expires.
+		  		So that we can force logout the client.
+		  	*/
+				if(status == 401){
+					$rootScope.$broadcast('forceLogout');
+				}
+		  	errorCallback(data);
+		  })
+	};
+	serverService.showGlobalError = function(){
+		/*
+			show some global error via some global application error showing mechanism 
+			(may be a toaster can be used)
+		*/
+	}
+	return serverService;
+});
